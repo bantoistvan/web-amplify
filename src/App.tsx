4ec0a345a318bad66f32b3b2a2204b206f67c700
Zoltan <zoltan.erdos@me.com>
2019-03-20 08:44:16 +0000

    add scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        {/* <header className="App-header"> */}
+          <ul className="main-nav">
+            <li>Features</li>
+            <li>Products</li>
+            <li>Blog</li>
+            <li className="main-nav--featured">Log In</li>
+            <li className="main-nav--featured">Sign Up</li>
+          </ul>  
           <p>
             zed.vision yeah
           </p>
-        </header>
+        {/* </header> */}
       </div>
     );
   }
