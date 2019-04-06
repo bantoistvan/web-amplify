2f8921603d1816c29e67a81640f2c9c494b3d52a
Zoltan <zoltan.erdos@me.com>
2019-04-06 17:12:25 +0100

    add graphql endpoint

---
+++ src/graphql/subscriptions.ts
@@ -0,0 +1,30 @@
+// tslint:disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = `subscription OnCreateTodo {
+  onCreateTodo {
+    id
+    name
+    email
+    message
+  }
+}
+`;
+export const onUpdateTodo = `subscription OnUpdateTodo {
+  onUpdateTodo {
+    id
+    name
+    email
+    message
+  }
+}
+`;
+export const onDeleteTodo = `subscription OnDeleteTodo {
+  onDeleteTodo {
+    id
+    name
+    email
+    message
+  }
+}
+`;
