c868863b0b584a427bac65c9f81401aa04411cb7
Istvan banto <bantoistvan@gmail.com>
2019-04-06 14:51:08 +0100

    New icon added

--- src/components/Logos.tsx
+++ src/components/Logos.tsx
@@ -1,6 +1,6 @@
 import React, { Component, ChangeEvent } from 'react';
 import { Paper, Typography, TextField, withStyles, Button, Icon, Grid } from '@material-ui/core';
-import { DockerIcon } from './icons';
+import { DockerIcon, NodeJsIcon } from './icons';
 
 const styles = {
     paper: {
@@ -28,7 +28,7 @@ class Logos extends Component<{ classes: any }> {
                 <table>
                     <tr>
                         <td className={classes.iconTable}><DockerIcon /></td>
-                        <td className={classes.iconTable}><DockerIcon /></td>
+                        <td className={classes.iconTable}><NodeJsIcon /></td>
                         <td className={classes.iconTable}><DockerIcon /></td>
                         <td className={classes.iconTable}><DockerIcon /></td>
                         <td className={classes.iconTable}><DockerIcon /></td>
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,1 +1,2 @@
 export * from "./DockerIcon";
+export * from "./NodeJsIcon";
