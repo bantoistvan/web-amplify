9521788b1c2084dee1a4dce789a8b2c6a084404c
Istvan banto <bantoistvan@gmail.com>
2019-04-06 16:35:52 +0100

    Fixed icons

--- src/components/icons/ServerlessIcon.tsx
+++ src/components/icons/ServerlessIcon.tsx
@@ -4,8 +4,12 @@ import { withStyles } from '@material-ui/core';
 const image = require('../../assets/icons/serverless.png');
 
 const styles = {
+    container: {
+        overflow: "hidden",
+        height: 60
+    },
     icon: {
-        width: 60
+        width: 60,
     }
 };
 
@@ -14,8 +18,11 @@ class Icon extends Component<{ classes: any }> {
     render() {
         const classes = this.props.classes;
 
-        return (<img className = {classes.icon}  src={image} alt="Serverless"/>
-        
+        return (
+            <div className={classes.container}>
+             <img className = {classes.icon}  src={image} alt="Serverless"/>
+        </div>
+
         );
     }
 }
